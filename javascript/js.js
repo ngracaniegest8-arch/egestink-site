2be31f1dd84ce268a06adf149d2b1fc13e3f91e8
egest <ngracaniegest8@gmail.com>
2025-11-03 17:35:37 +0200

    update index.html gallery.html js.js Drawing.html

--- javascript/js.js
+++ javascript/js.js
@@ -132,6 +132,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //pjesa e logos
 
-
+function toggleMenu() {
+  const sidebar = document.getElementById("sidebar");
+  if (sidebar.style.width === "250px") {
+    sidebar.style.width = "0";
+  } else {
+    sidebar.style.width = "250px";
+  }
+}
 
 
